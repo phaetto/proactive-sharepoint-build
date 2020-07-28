f113b8648e8904bbbdae442f04aabdbf45d15c74
Alexander Mantzoukas <ama@proactive.dk>
2020-07-28 12:41:15 +0200

    Split project in two to allow use of .NET Core fo the tool

--- ProActive.SharePoint.Build.Upload/Program.cs
+++ ProActive.SharePoint.Build.Upload/Program.cs
@@ -6,6 +6,7 @@
     using System;
     using System.IO;
     using System.IO.Compression;
+    using ProActive.SharePoint.Build.Console.Services;
 
     internal class Program
     {
@@ -26,48 +27,16 @@
             ConsoleExtensions.WriteLineWithColor("\n\n\tMade with <3 from ProActive - Contact: ama@proactive.dk\n\n", ConsoleColor.Cyan);
 
             var parser = new Parser(with => with.HelpWriter = Console.Out);
-            var parserResult = parser.ParseArguments<EmbedOptions, PackOptions>(args);
+            var parserResult = parser.ParseArguments<UploadOptions>(args);
             _ = parserResult
-                .WithParsed<PackOptions>(o =>
+                .WithParsed(o =>
                 {
-                    string sourceFolder = o.SourceFolder;
-                    string debugFolder = o.DebugFolder ?? Path.GetFullPath(Path.Combine(".", "dist", "debug"));
-                    string creationTemplatesFolder = string.IsNullOrWhiteSpace(o.TemplatesFolder)
-                        ? Path.GetFullPath(Path.Combine(".", "Templates", "Creation"))
-                        : Path.GetFullPath(o.TemplatesFolder);
-                    string sspkgFolder = o.SpfxFolder ?? Path.GetFullPath(".");
+                    Console.WriteLine("Uploading...");
 
-                    // Initialize environment
-                    var initSpfxFolderStructure = new InitSpfxFolderStructureService(sourceFolder, debugFolder);
-                    var applicationLoadContext = initSpfxFolderStructure.Process();
-
-                    Console.WriteLine("Creating spfx structure...");
-
-                    var createSpfxFolderStructure = new CreateSpfxFolderStructureService(debugFolder, creationTemplatesFolder, applicationLoadContext);
-                    createSpfxFolderStructure.Process();
-                    ConsoleExtensions.WriteLineWithColor("Done!", ConsoleColor.Green);
-
-                    Console.WriteLine("Creating artifacts...");
-
-                    // TODO: move content to argument
-                    var copyFilesToSpfxFolder = new CopyFilesToSpfxFolderService(sourceFolder, debugFolder, Path.GetFullPath(Path.Combine(".", "Content")), applicationLoadContext);
-                    copyFilesToSpfxFolder.Process();
-                    ConsoleExtensions.WriteLineWithColor("Done!", ConsoleColor.Green);
-
-                    Console.WriteLine("Creating zip archive...");
-
-                    var sspkgFile = Path.GetFullPath(Path.Combine(sspkgFolder, $"{applicationLoadContext.SharePointProduct.Name}.sspkg"));
-                    if (File.Exists(sspkgFile))
-                    {
-                        File.Delete(sspkgFile);
-                    }
-                    ZipFile.CreateFromDirectory(debugFolder, sspkgFile);
+                    var uploadAndDeployAppService = new UploadAndDeployAppService(o.SpfxFilePath, o.TenantName, o.ClientId, o.ClientSecret, o.SkipFeatureDeployment);
+                    uploadAndDeployAppService.Process();
 
                     ConsoleExtensions.WriteLineWithColor("Done!", ConsoleColor.Green);
-                })
-                .WithParsed<EmbedOptions>(o =>
-                {
-                    // TODO: use direct input and create an spfx
                 })
                 .WithNotParsed(errors =>
                 {
